3ca45019fced3468257d1a833ab50d3bc5483d00
Bret Comnes <bcomnes@gmail.com>
2014-12-18 17:28:45 -0800

    Added example file

---
+++ example/index.js
@@ -0,0 +1,28 @@
+var qGit = require('../');
+var path = require('path');
+
+var repoAPath = path.join(__dirname, 'repoA')
+var repoBPath = path.join(__dirname, 'repoB')
+
+var repoA = qGit(repoAPath);
+var repoB = qGit(repoBPath);
+
+repoA.init(function(err, stdout, stderr) {
+
+  console.log('Initialized new repository at ' + repoAPath)
+
+  repoB.clone(repoAPath, function(err, stdout, stderr) {
+
+    console.log('Cloned ' + repoAPath + ' to ' + repoBPath)
+
+    repoA('status', function(err, stdout, stderr) {
+      console.log('Ran `git status` in repoA:')
+      console.log(stdout);
+    });
+
+    repoB(['pull', 'origin'], function(err, stdout, stderr) {
+      console.log('repoB pulled repo A')
+      console.log(stdout);
+    });
+  });
+});
